b3589936d8d6492628de3aeefd86961f8a628fab
ElisaCarstairs <marina.arredondo.h@gmail.com>
2025-02-28 00:23:07 -0600

    Añadir clase2

---
+++ Clase2/Comparativa.js
@@ -0,0 +1,103 @@
+console.log("1er ejemplo");
+let Calif = 50; // Cambia este valor para probar diferentes notas
+
+if (Calif >= 60) {
+
+    if (Calif >= 75) {
+
+        if (Calif >= 90) {
+
+            if (Calif <= 100) {
+                console.log("Excelente.");
+            }
+        } else {
+            console.log("Has aprobado con Bien.");
+        }
+    } else {
+        console.log("Suficiente.");
+    }
+} else {
+    if (Calif < 60) {
+        console.log("No aprobado :(");
+    }
+}
+
+console.log(`Tu calificación es: ${Calif}`);
+
+console.log("2do ejemplo"); //2do ejemplo Calificación excelente
+
+ Calif = Calif +45; 
+
+if (Calif >= 60) {
+
+    if (Calif >= 75) {
+
+        if (Calif >= 90) {
+
+            if (Calif <= 100) {
+                console.log("Excelente.");
+            }
+        } else {
+            console.log("Has aprobado con Bien.");
+        }
+    } else {
+        console.log("Suficiente.");
+    }
+} else {
+    if (Calif < 60) {
+        console.log("No aprobado :(");
+    }
+}
+
+console.log(`Tu calificación es: ${Calif}`);
+
+//3er ejemplo 
+Calif = Calif -20; 
+
+if (Calif >= 60) {
+
+    if (Calif >= 75) {
+
+        if (Calif >= 90) {
+
+            if (Calif <= 100) {
+                console.log("Excelente.");
+            }
+        } else {
+            console.log("Bien.");
+        }
+    } else {
+        console.log("Suficiente.");
+    }
+} else {
+    if (Calif < 60) {
+        console.log("No aprobado :(");
+    }
+}
+
+console.log(`Tu calificación es: ${Calif}`);
+//4to ejemplo 
+Calif = Calif -14; 
+
+if (Calif >= 60) {
+
+    if (Calif >= 75) {
+
+        if (Calif >= 90) {
+
+            if (Calif <= 100) {
+                console.log("Excelente.");
+            }
+        } else {
+            console.log("Bien.");
+        }
+    } else {
+        console.log("Suficiente.");
+    }
+} else {
+    if (Calif < 60) {
+        console.log("No aprobado :(");
+    }
+}
+
+console.log(`Tu calificación es: ${Calif}`);
