a76b5496f2d6820407a816972111cbaef78f9686
ElisaCarstairs <marina.arredondo.h@gmail.com>
2025-02-27 22:59:23 -0600

    Añadir Clase2

---
+++ Tipos_de_datos.js
@@ -0,0 +1,20 @@
+console.log(typeof 42); // number
+console.log(typeof 'Veinticinco'); // string
+console.log(typeof -666); // number
+console.log(typeof true); // boolean
+console.log(typeof 0); // number
+console.log(typeof ''); // string
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+console.log(typeof false); // boolean
+
+console.log("Ejemplos adicionales;");
+console.log(typeof true); // boolean
+console.log(typeof 'Hola'); // string
+console.log(typeof null); // object
+console.log(typeof [1, 2, 3]); // object (arreglo en JavaScript es considerado un objeto)
+console.log(typeof { nombre: 'Juan', edad: 30 }); // object (un objeto)
+console.log(typeof function() {}); // function
+console.log(typeof Symbol('simbolo')); // symbol
+console.log(typeof BigInt(9007199254740991)); // bigint
+
